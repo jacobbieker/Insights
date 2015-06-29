23ba2531c1452d7adc0f53b7cc2676b4626d78ff
Jacob Bieker <jacob@bieker.us>
2015-06-29 07:53:27 -0700

    Add start of GitHub authentication

---
+++ GitHub2SQLite.py
@@ -0,0 +1,26 @@
+__author__ = 'Jacob Bieker'
+import github3
+import os
+import peewee
+import yaml
+
+def my_two_factor_function():
+    code = ''
+    while not code:
+        # The user could accidentally press Enter before being ready,
+        # let's protect them from doing that.
+        code = raw_input('Enter 2FA code: ')
+    return code
+
+#Authentication with Instagram
+with open("access.yaml", 'r') as access:
+    access_config = yaml.load(access)
+
+username = access_config['github']['username']
+password = access_config['github']['password']
+
+if access_config['github']['twoFactor']:
+    auth = github3.login(username=username, password=password,
+                  two_factor_callback=my_two_factor_function)
+else:
+    auth = github3.login(username=username, password=password)
