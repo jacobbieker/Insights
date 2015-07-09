da04ea6874c170fb11c762516d236a732320771a
Jacob Bieker <jacob@bieker.us>
2015-07-08 21:10:35 -0700

    Add basic locations opening, remove hardcoding in Gmail script

--- GoogleTakeoutScripts/Gmail2JSON.py
+++ GoogleTakeoutScripts/Gmail2JSON.py
@@ -12,8 +12,8 @@ import yaml
 with open("constants.yaml", 'r') as ymlfile:
     constants = yaml.load(ymlfile)
 
-MBOX = constants['dataDir'] + 'Mail\All mail Including Spam and Trash.mbox'
-OUT_FILE = constants['outputDir'] + "\gmail.json"
+MBOX = os.path.join(constants['dataDir'], 'Mail', 'All mail Including Spam and Trash.mbox')
+OUT_FILE = os.path.join(constants['outputDir'], "gmail.json")
 
 def cleanContent(msg):
 
--- GoogleTakeoutScripts/Location2SQLite.py
+++ GoogleTakeoutScripts/Location2SQLite.py
@@ -5,6 +5,11 @@ import os
 from datetime import datetime
 from peewee import *
 import setup
+from geopy.geocoders import Nominatim
 
 rootdir = os.path.join(setup.DATA_PATH, "Takeout", "Location")
+geolocator = Nominatim()
 
+with open(os.path.join(rootdir, "Location.json"), 'r') as source:
+    data = json.dump(source)
+    print data
