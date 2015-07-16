9eff0bf59d9ef6e655fd91211b0fe4bf2e6b9758
Jacob Bieker <jacob@bieker.us>
2015-07-15 18:24:46 -0700

    Remove hardcoding

--- LinkedIn2JSON.py
+++ LinkedIn2JSON.py
@@ -19,11 +19,12 @@ __author__ = 'Jacob'
 import csv
 import os
 import json
+import yaml
 
-'''
+
 with open("constants.yaml", 'r') as ymlfile:
     constants = yaml.load(ymlfile)
-'''
+
 
 rootdir = "C:\Users\jacob_000\OneDrive\Personal_Projects\LinkedIn"
 
@@ -32,10 +33,10 @@ for file in os.listdir(rootdir):
         #TODO Use config file for this?
         if(file == "Organizations.csv" or file == "Honors.csv" or file == "Education.csv" or file == "Positions.csv" or
                    file == "Projects.csv" or file == "Courses.csv"):
-            with open(rootdir + "\\" + file, 'r') as source:
+            with open(os.path.join(rootdir, file), 'r') as source:
                 file_name = os.path.splitext(os.path.basename(file))
                 csv_reader = csv.DictReader(x.replace('\0', '') for x in source)
-                with open(os.path.join('C:\Development\personal_analysis\output', 'linkedin.' + file_name[0] + '.json'), 'a') as json_output:
+                with open(os.path.join(constants.get("outputDir"), 'linkedin.' + file_name[0] + '.json'), 'a') as json_output:
                     for row in csv_reader:
                         json_data = row
                         #Output to file
