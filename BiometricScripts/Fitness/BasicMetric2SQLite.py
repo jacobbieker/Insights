1149726969dd4803272ddd3636ebf151e545cab6
Jacob Bieker <jacob@bieker.tech>
2016-02-19 22:46:17 -0800

    Add empty files to finish later

---
+++ BiometricScripts/Fitness/BasicMetric2SQLite.py
@@ -0,0 +1,5 @@
+__author__ = 'jacob'
+'''
+This script is designed to bring basic information (height, age, etc.) into the database. Basically, this script should
+deal with data that does not usually change much over time.
+'''
