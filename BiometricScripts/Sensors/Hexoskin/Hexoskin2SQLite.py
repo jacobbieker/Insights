5b80ac3b7962f5207a6194f2f31f8e8034509c5d
Jacob Bieker <jacob@bieker.tech>
2017-07-21 18:07:30 +0200

    Add Hexoskin API 2.0 example code
    Taken from their Bitbucket account, should allow access to raw data files

--- BiometricScripts/Sensors/Hexoskin/Hexoskin2SQLite.py
+++ BiometricScripts/Sensors/Hexoskin/Hexoskin2SQLite.py
@@ -1,1 +1,32 @@
 __author__ = 'jacob'
+'''
+Copyright (C) 2017  Jacob Bieker, jacob@bieker.us, www.jacobbieker.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
+import os
+import peewee
+import requests
+import yaml
+
+#Authentication with Instagram
+# Have to do this because when the command is called from the import in any subfolder it cannot find the dbconfig
+if __name__ != "__main__":
+    with open(os.path.join("access.yaml"), 'r') as access:
+        access_config = yaml.load(access)
+else:
+    with open(os.path.join("access.yaml"), 'r') as access:
+        access_config = yaml.load(access)
